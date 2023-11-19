5542ec027a414230333b8924d7ad351ec72b4979
Sultan-Orunov <osbfswd@gmail.com>
2023-11-19 23:03:35 +0500

    create Layout

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>Index Page</div>
-    <Link href="/hello">Show Page</Link>
-    <div>message from controller: {{ message }}</div>
+    <MainLayout>
+        <div>Index Page</div>
+        <Link href="/hello">Show Page</Link>
+        <div>message from controller: {{ message }}</div>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import MainLayout from "../Layouts/MainLayout.vue";
 defineProps(["message"]);
 </script>
---
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Link href="/">Index Page</Link>&nbsp;
+        <Link href="/hello">Show Page</Link>
+
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
